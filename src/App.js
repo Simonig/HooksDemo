1d1ce71e4df70f6ba25130801b73af0beec9dc46
simon.godoy <simon.godoy@check24.de>
2019-01-17 13:29:40 +0100

    Remove unnecesary code

--- src/App.js
+++ src/App.js
@@ -1,62 +1,11 @@
-import React, { Component} from 'react';
+import React from 'react';
 import './App.css';
 import AddDocuments from './AddDocuments';
 import DocumentList from './DocumentList';
-import { set, remove } from 'immutable';
 import withDocumentIds from './withDocumentIds';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      documentIds: [],
-    }
-  }
 
-  onAddDocument = (newDocId) => {
-    const {documentIds} = this.state;
-    if(!documentIds.includes(newDocId)){
-      this.setState({documentIds: [...documentIds, newDocId]})
-    }
-  }
-  
-  onEditDocument = (i, newDocId) => {
-    const {documentIds} = this.state;
-    if(!documentIds.includes(newDocId)){
-        this.setState({documentIds: set(this.state.documentIds, i, newDocId)})
-    }
-  }
-
-  onRemoveDocument = (i) => {
-    const {documentIds} = this.state; 
-    if(documentIds) this.setState({documentIds: remove(this.state.documentIds, i)})
-  }
-
-  render() {
-    return (
-      <div className="App">
-        
-        <AddDocuments handleSubmit={this.onAddDocument} text="Search Documents" className="searchDocs"/>
-
-        <div className="documentListWrapper">
-          {this.state.documentIds && this.state.documentIds.map((docId, i) => (  
-            <div key={docId} className="documentList">
-              
-              <DocumentList  
-                docId={docId} 
-                handleChangeId={(newDocId) => this.onEditDocument(i, newDocId)}
-                handleRemove={()=> this.onRemoveDocument(i)}
-              />
-            
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
-}
-
-const HocApp = ({addDocumentId, documentIds, editDocumentId, removeDocumentId}) => {
+const App = ({addDocumentId, documentIds, editDocumentId, removeDocumentId}) => {
   return (
       <div className="App">
           
@@ -79,4 +28,5 @@ const HocApp = ({addDocumentId, documentIds, editDocumentId, removeDocumentId}) 
   )
 }
 
-export default withDocumentIds(HocApp);
+
+export default withDocumentIds(App);
