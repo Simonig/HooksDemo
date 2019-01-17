dc2ec1ffd68c5992fce81a6d86b16f70566bc811
simon.godoy <simon.godoy@check24.de>
2019-01-17 14:19:01 +0100

    With DocumentList Hooks

--- src/DocumentList.js
+++ src/DocumentList.js
@@ -1,58 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import DocumentsApi from "./DocumentsApi"
 import AddDocuments from "./AddDocuments";
 import DocumentItem from "./DocumentItem";
 
-class DocumentList extends React.Component {
-    constructor(props){
-        super(props)
-        this.docApi = DocumentsApi(props.docId)
-        this.state = {
-            documents: []
-        }
-    }
 
-    componentDidMount(){
-        this.docApi.subscribeToDocuments(this.handleNewDocs)
-    }
+const useDocuments = (docId) => {
+    const [documents, setDocuments] = useState([])
+    useEffect(()=> {
+        const docListener = DocumentsApi(docId)
+        const handleReceiveDocuments = (newDoc) => setDocuments(newDoc.detail)
+        
+        docListener.subscribeToDocuments(handleReceiveDocuments)
 
-    componentWillUnmount(){
-        this.docApi.unsubscribeToDocuments(this.handleNewDocs)
-    }
-
-    componentDidUpdate(prevProps){
-        const {docId} = this.props;
-        if(docId !== prevProps.docId){
-            this.docApi.unsubscribeToDocuments(this.handleNewDocs)
-            this.docApi = DocumentsApi(docId)
-            this.docApi.subscribeToDocuments(this.handleNewDocs)
-        }
-    }
-
-    handleNewDocs = (newDoc) => {
-        this.setState({documents: newDoc.detail})
-    }
-
-    render(){
-        const {docId, handleChangeId, handleRemove} = this.props;
-        return(
-            <React.Fragment >
-                <div className="documentListHeader">
-                    <p> Document: {docId}                 
-                        <span className="removeBtn" onClick={handleRemove}>
-                            Remove
-                        </span>
-                    </p>
-                    <AddDocuments  handleSubmit={handleChangeId} text={`Change Id`}/>
-                </div>
-
-                <div className="documentComponent">
-                    {this.state.documents && this.state.documents.map((doc, i) => <DocumentItem key={i} {...doc} />) }
-                </div>
-            </React.Fragment>
-        )
-    }
+        return () => docListener.unsubscribeToDocuments(handleReceiveDocuments)
+    }, [docId])
+    
+    return documents
 }
+
+
+const DocumentList = ({docId, handleChangeId, handleRemove}) => {
+    const documents = useDocuments(docId);
+    return(
+        <React.Fragment >
+            <div className="documentListHeader">
+                <p> Document: {docId}                 
+                    <span className="removeBtn" onClick={handleRemove}>
+                        Remove
+                    </span>
+                </p>
+                <AddDocuments handleSubmit={handleChangeId} text={`Change Id`}/>
+            </div>
+            <div className="documentComponent">
+                {documents && documents.map((doc, i) => <DocumentItem key={i} {...doc} />) }
+            </div>
+        </React.Fragment>
+    )
+}
+
 
 
 export default DocumentList
